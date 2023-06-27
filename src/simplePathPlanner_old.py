d5c390837b1c4ec7595b94f9279cd03582607f0f
DDorner <ddorner@kth.se>
2023-06-27 19:18:37 +0200

    Code Refactoring for TDD
    Refactoring the simplePathPlanner for TDD
    - Splitting ROS-related from algorithmic related code
    - Implementing tests for each function

--- src/simplePathPlanner_old.py
+++ src/simplePathPlanner_old.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 # Copyright 2023 David Doerner (ddorner@kth.se)
 
+# Bezier implementation from
+# https://towardsdatascience.com/b%C3%A9zier-curve-bfffdadea212
+
 from __future__ import division, print_function
 
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import os 
+
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray, Pose, PoseStamped
 
 from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_matrix, quaternion_from_matrix
 import tf
 
+
 class SimplePathPlanner(object):
     def __init__(self, name):
+
+        # External Parameter
+        self.loop_freq = rospy.get_param("~loop_freq", 20)
+        rate = rospy.Rate(self.loop_freq) 
+        verbose = rospy.get_param("~verbose", True)
 
         # Init
         # map frame
@@ -52,10 +64,177 @@ class SimplePathPlanner(object):
 
         self.currentWaypoint = [0., 0.]
 
+        # Topics
+        dockingStationEstimateTopic = rospy.get_param("~dockingStationEstimationTopic")
+        stateEstimateTopic = rospy.get_param("~stateEstimationTopic")
+        waypointTopic = rospy.get_param("~waypointTopic")
+
+        # Subscribers
+        rospy.Subscriber(dockingStationEstimateTopic, PoseWithCovarianceStamped, self.dockingStationEstimateCallback, queue_size=1)
+        rospy.Subscriber(stateEstimateTopic, PoseWithCovarianceStamped, self.stateEstimateCallback, queue_size=1)
+
+        # Publisher
+        self.waypointPub = rospy.Publisher(waypointTopic, PoseWithCovarianceStamped, queue_size=1)
+
         # TF tree listener
-        # TODO: Put that into the corresponding ROS node
-        # self.listener = tf.TransformListener()
-        # self.base_frame = 'sam/base_link/estimated'
+        self.listener = tf.TransformListener()
+        self.base_frame = 'sam/base_link/estimated'
+
+        # Run
+        while not rospy.is_shutdown():
+            print("Receiving goal?")
+            if self.receivedGoal:
+                # 1. Check if you need to calculate a plan
+                print("Checking for plan")
+                planNeeded = self.requirePlan()
+                # 1.1 Yes: Calculate Plan
+                if planNeeded:
+                    print("Planning")
+                    if self.inFeasibleRegion():
+                        self.calculatePathSegments()
+                        self.calculateBezierCurve()
+                        self.calculatedPath = True
+
+                        # Plot Path -> saves to file
+                        self.plotPath()
+                        self.plotTFSamBase()
+                    else:
+                        self.plotPosition()
+                # 1.2 No: continue
+
+                if self.calculatedPath:
+                    # 2. Get current waypoint
+                    self.getCurrentWaypoint()
+
+                    # 3. Publish current waypoint
+                    self.publishCurrentWaypoint()
+            # else:
+                # print("No goal available")
+
+            if verbose:
+                self.printStates()
+
+            # 4. Sleep
+            rate.sleep()
+
+
+    #region Callbacks
+    def dockingStationEstimateCallback(self,DSEstim):
+        # map frame
+        DSPose = self.getEulerFromQuaternion(DSEstim.pose)
+        self.goal[0] = DSPose[0]
+        self.goal[1] = DSPose[1]
+        self.goal[2] = DSPose[5]
+
+
+        xAxisDS, yAxisDS = self.calculateOrientationAxes(self.goal[2], -1)
+        self.target = np.array([self.goal[0] + xAxisDS[0], self.goal[1] + xAxisDS[1]])
+
+        targetMap = PoseWithCovarianceStamped()
+        targetMap.header.frame_id = 'map'
+        targetMap.header.stamp = rospy.Time(0)
+        targetMap.pose.pose.position.x = self.target[0]
+        targetMap.pose.pose.position.y = self.target[1]
+        targetMap.pose.pose.position.z = 0.
+        targetMap.pose.pose.orientation.x = DSEstim.pose.pose.orientation.x
+        targetMap.pose.pose.orientation.y = DSEstim.pose.pose.orientation.y
+        targetMap.pose.pose.orientation.z = DSEstim.pose.pose.orientation.z
+        targetMap.pose.pose.orientation.w = DSEstim.pose.pose.orientation.w
+                     
+        self.goalBase = self.tf2SamBase(DSEstim)
+        self.targetBase = self.tf2SamBase(targetMap)
+        
+        self.receivedGoal = True
+
+    def tf2SamBase(self,poseArg):
+        poseBase = np.array([0., 0., 0.])
+
+        # Transform into sam/baselink
+        tmpPose = PoseStamped()
+        tmpPose.header.frame_id = 'map'
+        tmpPose.header.stamp = rospy.Time(0)
+        tmpPose.pose.position.x = poseArg.pose.pose.position.x
+        tmpPose.pose.position.y = poseArg.pose.pose.position.y
+        tmpPose.pose.position.z = poseArg.pose.pose.position.z
+        tmpPose.pose.orientation.x = poseArg.pose.pose.orientation.x
+        tmpPose.pose.orientation.y = poseArg.pose.pose.orientation.y
+        tmpPose.pose.orientation.z = poseArg.pose.pose.orientation.z
+        tmpPose.pose.orientation.w = poseArg.pose.pose.orientation.w
+
+        try:
+            goalSamBaseLink = self.listener.transformPose(self.base_frame, tmpPose)
+
+            poseBase[0] = goalSamBaseLink.pose.position.x
+            poseBase[1] = goalSamBaseLink.pose.position.y
+
+            rpy = euler_from_quaternion([goalSamBaseLink.pose.orientation.x,
+                                         goalSamBaseLink.pose.orientation.y,
+                                         goalSamBaseLink.pose.orientation.z,
+                                         goalSamBaseLink.pose.orientation.w])
+            poseBase[2] = rpy[2]
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logwarn("[SPP]: Transform to base frame not available yet")
+            pass
+
+        return poseBase
+
+
+    def tf2Map(self, poseBase):
+        poseMap = PoseStamped()
+
+        # Transform into map
+        tmpPose = PoseStamped()
+        tmpPose.header.frame_id = poseBase.header.frame_id
+        tmpPose.header.stamp = rospy.Time(0)
+        tmpPose.pose.position.x = poseBase.pose.pose.position.x
+        tmpPose.pose.position.y = poseBase.pose.pose.position.y
+        tmpPose.pose.position.z = poseBase.pose.pose.position.z
+        tmpPose.pose.orientation.x = poseBase.pose.pose.orientation.x
+        tmpPose.pose.orientation.y = poseBase.pose.pose.orientation.y
+        tmpPose.pose.orientation.z = poseBase.pose.pose.orientation.z
+        tmpPose.pose.orientation.w = poseBase.pose.pose.orientation.w
+
+        try:
+            poseMap = self.listener.transformPose('map', tmpPose)
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logwarn("Transform to base frame not available yet")
+            pass
+
+        return poseMap
+
+
+    def stateEstimateCallback(self,estim):
+        SAMPose = self.getEulerFromQuaternion(estim.pose)
+        self.start[0] = SAMPose[0]
+        self.start[1] = SAMPose[1]
+        self.start[2] = SAMPose[5]
+
+
+
+    def getEulerFromQuaternion(self, pose):
+        # Pose is in ENU. The callback extracts the position and Euler angles.
+        #
+        # Pose in ENU 
+        xENU = pose.pose.position.x
+        yENU = pose.pose.position.y
+        zENU = pose.pose.position.z
+
+        eta0ENU = pose.pose.orientation.w
+        eta1ENU = pose.pose.orientation.x
+        eta2ENU = pose.pose.orientation.y
+        eta3ENU = pose.pose.orientation.z
+        
+        rpyENU = euler_from_quaternion([eta1ENU,eta2ENU,eta3ENU,eta0ENU])
+        
+        rollENU = rpyENU[0]
+        pitchENU = rpyENU[1]
+        yawENU = rpyENU[2]
+
+        states = np.array([xENU, yENU, zENU, rollENU, pitchENU, yawENU])
+
+        return states
+    #endregion
 
     #region functions
     def requirePlan(self):
@@ -106,11 +285,10 @@ class SimplePathPlanner(object):
         self.controlPoints["theta"] = np.array([0., 0., self.goalBase[2]])
         
         # Transform into map frame
-        # TODO: This is in the ROS node again
-        # for pt in range(len(self.controlPoints["x"])):
-        #     controlpointPose = self.waypoint2pose([self.controlPoints["x"][pt], self.controlPoints["y"][pt]])
-        #     controlpointMap = self.tf2Map(controlpointPose)
-        #     self.controlPointsMap["x"][pt], self.controlPointsMap["y"][pt] = controlpointMap.pose.position.x, controlpointMap.pose.position.y
+        for pt in range(len(self.controlPoints["x"])):
+            controlpointPose = self.waypoint2pose([self.controlPoints["x"][pt], self.controlPoints["y"][pt]])
+            controlpointMap = self.tf2Map(controlpointPose)
+            self.controlPointsMap["x"][pt], self.controlPointsMap["y"][pt] = controlpointMap.pose.position.x, controlpointMap.pose.position.y
 
 
     def calculateBezierCurve(self):
@@ -124,11 +302,10 @@ class SimplePathPlanner(object):
         self.path["x"], self.path["y"] = points[:,0], points[:,1]        
 
         # Transform into map frame
-        # TODO: This is in the ROS node again
-        # for pt in range(len(self.path["x"])):
-        #     waypointPose = self.waypoint2pose([self.path["x"][pt], self.path["y"][pt]])
-        #     waypointMap = self.tf2Map(waypointPose)
-        #     self.pathMap["x"][pt], self.pathMap["y"][pt] = waypointMap.pose.position.x, waypointMap.pose.position.y
+        for pt in range(len(self.path["x"])):
+            waypointPose = self.waypoint2pose([self.path["x"][pt], self.path["y"][pt]])
+            waypointMap = self.tf2Map(waypointPose)
+            self.pathMap["x"][pt], self.pathMap["y"][pt] = waypointMap.pose.position.x, waypointMap.pose.position.y
 
 
     def getCurrentWaypoint(self):
@@ -144,22 +321,46 @@ class SimplePathPlanner(object):
         xDiff = self.currentWaypoint[0] - self.start[0]
         yDiff = self.currentWaypoint[1] - self.start[1]
         distance = np.linalg.norm([xDiff, yDiff])
-        return distance    
-    
-    # TODO: Put that in the ROS Node
-    # def waypoint2pose(self, waypoint):
-    #     poseWaypoint = PoseWithCovarianceStamped()
-    #     poseWaypoint.header.frame_id = self.base_frame
-    #     poseWaypoint.header.stamp = rospy.Time(0)
-    #     poseWaypoint.pose.pose.position.x = waypoint[0]
-    #     poseWaypoint.pose.pose.position.y = waypoint[1]
-    #     poseWaypoint.pose.pose.position.z = 0
-    #     poseWaypoint.pose.pose.orientation.w = 0
-    #     poseWaypoint.pose.pose.orientation.x = 0
-    #     poseWaypoint.pose.pose.orientation.y = 0
-    #     poseWaypoint.pose.pose.orientation.z = 0
+        return distance
 
-    #     return poseWaypoint
+
+    def publishCurrentWaypoint(self):        
+        try:
+            currentWaypointBase = self.waypoint2pose(self.currentWaypoint)
+            tmpWaypoint = self.tf2Map(currentWaypointBase)
+
+            # We always publish PoseWithCovarianceStamped, but tf.listener only can transform PoseStamped. 
+            publishWaypoint = PoseWithCovarianceStamped()
+            publishWaypoint.header.frame_id = 'map'
+            publishWaypoint.header.stamp = rospy.Time(0)
+            publishWaypoint.pose.pose.position.x = tmpWaypoint.pose.position.x
+            publishWaypoint.pose.pose.position.y = tmpWaypoint.pose.position.y
+            publishWaypoint.pose.pose.position.z = tmpWaypoint.pose.position.z
+            publishWaypoint.pose.pose.orientation.x = tmpWaypoint.pose.orientation.x
+            publishWaypoint.pose.pose.orientation.y = tmpWaypoint.pose.orientation.y
+            publishWaypoint.pose.pose.orientation.z = tmpWaypoint.pose.orientation.z
+            publishWaypoint.pose.pose.orientation.w = tmpWaypoint.pose.orientation.w
+
+            self.waypointPub.publish(publishWaypoint)
+            
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logwarn("map frame not available yet")
+            pass
+
+    
+    def waypoint2pose(self, waypoint):
+        poseWaypoint = PoseWithCovarianceStamped()
+        poseWaypoint.header.frame_id = self.base_frame
+        poseWaypoint.header.stamp = rospy.Time(0)
+        poseWaypoint.pose.pose.position.x = waypoint[0]
+        poseWaypoint.pose.pose.position.y = waypoint[1]
+        poseWaypoint.pose.pose.position.z = 0
+        poseWaypoint.pose.pose.orientation.w = 0
+        poseWaypoint.pose.pose.orientation.x = 0
+        poseWaypoint.pose.pose.orientation.y = 0
+        poseWaypoint.pose.pose.orientation.z = 0
+
+        return poseWaypoint
 
     #region Plotting
     def plotPath(self):
@@ -296,9 +497,8 @@ class SimplePathPlanner(object):
 
     #endregion
 
-    #region Outward-facing stuff
-    # The callbacks set the corresponding variables here
-    # Then you have the corresponding functions that return whatever and the ROS node can convert 
-    # it into the proper frame etc.
+if __name__ == "__main__":
+    rospy.init_node("SimplePathPlanner")
+    pathPlanner = SimplePathPlanner(rospy.get_name())
 
-    #endregion
+    
